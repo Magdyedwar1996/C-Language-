b408d9e7f22c7fdfdc80cc85fb7a30535199a2af
magdy_edwar <magdyedwar1996@gmail.com>
2021-07-09 16:02:47 +0200

    this code is to find the table of multiplication of a number

---
+++ 02_NTI Course/01_ Week one/02_some excercises/multiplication_table_for_number.c
@@ -0,0 +1,21 @@
+/****************************************************************/
+/* Auter   	   : Magdy Edwar	 								*/
+/* Version 	   : V01											*/
+/* Date    	   : 5 Jul 											*/
+/* Discription : 					                  			*/
+/****************************************************************/
+#include <stdio.h>
+#include <>
+void main(void)
+{
+
+	int num;
+	printf("plz enter an integer number: ");
+	scanf("%d", &num);
+	
+	for(int i=0; i<=12 ; i++)
+	{
+		printf("%d x %d = %d\n", i, num, (i*num) );
+	}
+
+}
