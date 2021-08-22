8968060f6993ce43e8f18a331108c93b2495f35f
Magdy Edwar <magdiedwar47@gmail.com>
2021-08-22 21:14:52 +0200

    This is implementation of a function that takes a string and returns the first repeated character in this string

---
+++ 03_ Introductory C Programming Specialization/01_Course one  Programming Fundamentals/01_Course notes/TheFirstRepeatedElemnet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char FirstRepeatedElement(char* string)
+{
+	int count[256]={0};
+	for (int i = 0; ; i++)
+		{
+			count[string[i]]++;
+			if(count[	[i]] == 2)
+			{
+				printf("The index of the first repeated character in the string is %d\n",i);
+				return string[i];
+			}	
+		}
+ }
+
+int main() {
+
+	char string[]= "magdy is an engineer and wants to work in amazon";
+	//int size = sizeof(arr)/ sizeof(char);
+	char character = FirstRepeatedElement(string );
+	 printf("The most repeated character in the string is : %c \n",character);
+		return 0;
+}
